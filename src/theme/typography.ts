fdf6b2b4f99e846217cb2c067160b9a699a9458e
E Rodrigo Lopes <e.rodrigo.l@outlook.com>
2025-03-29 18:47:51 -0300

    feat: integradas fontes customizadas com `expo-font` e ajustes de estilização em `SettingsScreen`
    - instalado `npm install expo-font` para uso de fontes locais
    - ajustado carregamento dinâmico das fontes a partir de `assets/fonts/`
    - corrigida estilização dos componentes `SelectList` com fonte, tamanho e cor aplicados dinamicamente

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -2,16 +2,16 @@
 const typography = {
   // Definição das famílias de fontes disponíveis
   fonts: {
-    arial: "Arial", // Fonte Arial
-    times: "Times New Roman", // Fonte Times New Roman
-    openSans: "Open Sans", // Fonte Open Sans
+    arial: "Arial",
+    roboto: "Roboto",
+    times: "Times New Roman",
   },
 
   // Definição dos tamanhos de texto e multiplicadores
   sizes: {
     body: 16, // Tamanho base do corpo do texto em pixels
-    subtitleMultiplier: 1.2, // Multiplicador para o tamanho do subtítulo (20% maior que o corpo)
-    titleMultiplier: 1.5, // Multiplicador para o tamanho do título (50% maior que o corpo)
+    subtitleMultiplier: 1.3, // Multiplicador para o tamanho do subtítulo (30% maior que o corpo)
+    titleMultiplier: 1.6, // Multiplicador para o tamanho do título (60% maior que o corpo)
     min: 12, // Tamanho mínimo permitido para textos em pixels
     max: 24, // Tamanho máximo permitido para textos em pixels
   },
